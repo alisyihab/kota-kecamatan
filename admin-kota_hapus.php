0df9843a4ad2dbfcea8c99ec7b48e19ffec6a03e
randi <randiansyah1@gmail.com>
2019-04-09 19:15:20 +0700

    menghapus file file takperlu

--- admin-kota_hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-   
-    require_once('konfigurasi.php');
-
-    $deletesql = "DELETE FROM kota    
-    WHERE id = '{$_GET['kotaid']}'";
-    $koneksidb->exec($deletesql);
-    header('Location: admin-kota_daftar.php');
-?>
